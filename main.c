0ad1df6f3dd88e640bce3d9722af867cb9ab21b6
Sam Meech-Ward <sam@meech-ward.com>
2017-07-24 10:31:23 -0700

    Add a fizzbuzz algorithm

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+bool isMultipleOf(int i, int multiple) {
+    return i & multiple == 0;
+}
+
 int main(int argc, const char * argv[]) {
     
+    for (int i = 0; i <= 100; ++i) {
+        if (isMultipleOf(i, 3) && isMultipleOf(i, 5)) {
+            printf("Fizz Buzz\n");
+        } else if (isMultipleOf(i, 3)) {
+            printf("Fizz\n");
+        } else {
+            printf("Buzz\n");
+        }
+    }
     
     return 0;
 }
